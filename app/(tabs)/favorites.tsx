00f369f44189cbcc4b7a2ce35b589c589b2ce80f
meandrewaprianto <devandrew.aprianto@gmail.com>
2026-02-28 18:12:47 +0700

    Fresh Expo app with nativewind

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+const Favorites = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Favorites</Text>
         </View>
     )
 }
 
-export default Home
+export default Favorites
